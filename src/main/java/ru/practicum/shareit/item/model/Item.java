8b2902746a51501c88101bce3d14ee7fe731b533
Евгений <89537796626@mail.ru>
2023-07-05 15:59:40 +0700

    Реализация модели данных

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,25 @@
 package ru.practicum.shareit.item.model;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
+@RequiredArgsConstructor
 public class Item {
+    //уникальный идентификатор вещи.
+    private int id;
+    //краткое название.
+    private String name;
+    //развёрнутое описание.
+    private String description;
+    //статус о том, доступна или нет вещь для аренды.
+    private boolean available;
+    //владелец вещи.
+    private String owner;
+    /*если вещь была создана по запросу другого пользователя, то в этом
+      поле будет храниться ссылка на соответствующий запрос*/
+    private String request;
 }
--- src/main/java/ru/practicum/shareit/request/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequest.java
@@ -1,7 +1,22 @@
 package ru.practicum.shareit.request;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDate;
+
 /**
  * TODO Sprint add-item-requests.
  */
+@Data
+@RequiredArgsConstructor
 public class ItemRequest {
+    //уникальный идентификатор запроса.
+    private int id;
+    //текст запроса, содержащий описание требуемой вещи.
+    private String description;
+    //пользователь, создавший запрос.
+    private String requestor;
+    //дата и время создания запроса.
+    private LocalDate created;
 }
