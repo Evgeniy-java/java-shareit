c5dcdcd0d6f94d38ed881ddb7194b4040d111ba1
Евгений <89537796626@mail.ru>
2023-07-12 08:52:12 +0700

    убрал анотации Hibernate Validator в классе Item

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,12 +1,11 @@
 package ru.practicum.shareit.item.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import ru.practicum.shareit.request.ItemRequest;
 import ru.practicum.shareit.user.model.User;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 /**
  * TODO Sprint add-controllers.
@@ -19,14 +18,10 @@ public class Item {
     //уникальный идентификатор вещи.
     private Long id;
     //краткое название.
-    @NotBlank(message = "Не заполнено название вещи")
     private String name;
     //развёрнутое описание.
-    @NotBlank(message = "Не заполнено описание вещи")
-    @Size(max = 240, message = "В описании не более 240 символов")
     private String description;
     //статус о том, доступна или нет вещь для аренды.
-    @NotNull(message = "Не проставлен статус вещи")
     private Boolean available;
     //владелец вещи.
     private User owner;
